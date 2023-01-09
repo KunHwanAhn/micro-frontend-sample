40268af78f6f736907e03406827050c4c07baf26
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-09 19:35:37 +0900

    Container App 추가 (#2)
    * Remote App 설정 변경
    
    * Container App 추가

---
+++ app1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <div style={{
+        padding: '1rem',
+        textAlign: 'center',
+        backgroundColor: 'greenyellow',
+      }}>
+        <h1>Container App</h1>
+      </div>
+    </>
+  );
+}
